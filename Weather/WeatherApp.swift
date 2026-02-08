5143cfb24f4c25cb1f97362b585d302edc298b6f
aiueo <uaoi@icloud.com>
2026-02-08 11:55:10 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Aoi Ueno on 2026/02/08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
